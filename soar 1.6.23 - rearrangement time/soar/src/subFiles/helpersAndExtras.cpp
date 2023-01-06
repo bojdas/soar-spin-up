3d3872a21473e230467545c0a32338a3a1f5f9b1
bojro das <77548353+bojdas@users.noreply.github.com>
2023-01-06 11:17:19 -0500

    Update and rename joystickMapping.cpp to helpersAndExtras.cpp
    added slew rate code

--- soar 1.6.23 - rearrangement time/soar/src/subFiles/helpersAndExtras.cpp
+++ soar 1.6.23 - rearrangement time/soar/src/subFiles/helpersAndExtras.cpp
@@ -25,8 +25,26 @@ double trial(int i, bool side) {
 void Mapping(int i, bool side) {
     double t1 = 0, t2 = 0, t3 = 0;
     t1 = trial(i, side);
+    while (cece.get_digital(E_CONTROLLER_DIGITAL_A)) {
+    }
     t2 = trial(i, side);
+    while (cece.get_digital(E_CONTROLLER_DIGITAL_A)) {
+    }
     t3 = trial(i, side);
     double avg = (t1 + t2 +t3)/3.0;
     cece.print(0, 0, "rpm: %d", avg);
+}
+
+int slewRate() {
+  int lError = 0, rError = 0, kS = 0.25, placedLeft = 0, placedRight = 0;
+  while(true) {
+    lError = left - (lBack + lMid + lFront)/3;
+    rError = right - (rBack + rMid + rFront)/3;
+    placedLeft += lError * kS;
+    placedRight += rError *kS;
+
+    rightDrive = placedRight;
+    leftDrive = placedLeft;
+    pros::delay(25);
+  }
 }

ba68337cb95673d7a56f53f72e1ed13cfb1e4045
bojro das <77548353+bojdas@users.noreply.github.com>
2023-01-06 11:24:18 -0500

    Update helpersAndExtras.hpp
    moving helpers

--- soar 1.6.23 - rearrangement time/soar/include/subHeaders/helpersAndExtras.hpp
+++ soar 1.6.23 - rearrangement time/soar/include/subHeaders/helpersAndExtras.hpp
@@ -1,6 +1,14 @@
 #pragma once
 #include "main.h"
 
+//helpers
+void resetEncoders();
+double avgEncoderValue();
+void setDrive(int left, int right);
+
+//joystick mapping
 double trial(int i, bool side);
 void Mapping(int i, bool side);
+
+//slew rate
 int slewRate();

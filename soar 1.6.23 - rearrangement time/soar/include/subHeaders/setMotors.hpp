8f337caecc6f2de2fd2e65c2057fe7535975ff68
bojro das <77548353+bojdas@users.noreply.github.com>
2023-01-06 11:25:13 -0500

    Update setMotors.hpp
    removing helpers

--- soar 1.6.23 - rearrangement time/soar/include/subHeaders/setMotors.hpp
+++ soar 1.6.23 - rearrangement time/soar/include/subHeaders/setMotors.hpp
@@ -1,15 +1,10 @@
 #pragma once
 #include "main.h"
 
-//helpers
-void resetEncoders();
-double avgEncoderValue();
-
 //cata
 void setCataMotor(bool autoReload);
 
 //drive
-void setDrive(int left, int right);
 void setDriveMotors();
 
 //intake
